010805f5ad65c033ad897ec49c9bc5ba5482771d
Le Nhut Anh <leenanhh19@gmail.com>
2025-09-11 16:06:47 +0700

    feat: add bookGallery in bookDetail

---
+++ src/app/book/_components/book-gallery.tsx
@@ -0,0 +1,63 @@
+'use client';
+import { useState } from 'react';
+import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+import { Navigation, Thumbs } from 'swiper/modules';
+import type { Swiper as SwiperType } from 'swiper';
+
+import { product, banner1, banner2 } from '@/assets';
+
+export default function BookGallery() {
+  const [thumbsSwiper, setThumbsSwiper] = useState<SwiperType | null>(null);
+
+  const images = [product, banner1, banner2, product, product];
+
+  return (
+    <div className='w-full'>
+      <Swiper
+        modules={[Navigation, Thumbs]}
+        navigation
+        thumbs={{ swiper: thumbsSwiper }}
+        className='mb-8 w-full'
+      >
+        {images.map((img, index) => (
+          <SwiperSlide key={index}>
+            <div className='relative h-[440px] w-full'>
+              <Image
+                src={img}
+                alt={`Cover ${index}`}
+                fill
+                className='rounded-xl object-cover'
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <Swiper
+        modules={[Thumbs]}
+        onSwiper={setThumbsSwiper}
+        slidesPerView={5}
+        spaceBetween={10}
+        watchSlidesProgress
+        className='w-full'
+      >
+        {images.map((img, index) => (
+          <SwiperSlide key={index}>
+            <div className='relative h-[80px] w-full cursor-pointer'>
+              <Image
+                src={img}
+                alt={`Thumbnail ${index}`}
+                fill
+                className='rounded object-cover'
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
