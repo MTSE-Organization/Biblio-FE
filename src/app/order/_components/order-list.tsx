d0404c8e971a91b52b629dbfc2f07b6ba9a5241d
LeTanTru <letantru@gmail.com>
2025-09-20 22:27:29 +0700

    feat: order list

--- src/app/order/_components/order-list.tsx
+++
@@ -1,5 +0,0 @@
-'use client';
-
-export default function OrderList() {
-  return <div></div>;
-}
--- src/app/order/page.tsx
+++
@@ -1,5 +0,0 @@
-import OrderList from '@/app/order/_components/order-list';
-
-export default function OrderPage() {
-  return <OrderList />;
-}
