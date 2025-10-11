01d99fef43cbbf8d7b12283de33cffc85e574e0e
LeTanTru <letantru@gmail.com>
2025-10-11 08:54:29 +0700

    feat: box shadow review modal

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -64,7 +64,7 @@ export default function Modal({
           )}
 
           <motion.div
-            className='content relative rounded-lg bg-white shadow-lg'
+            className='content relative rounded-lg bg-white shadow-[0px_0px_10px_2px] shadow-black/40'
             initial={variants.initial}
             animate={variants.animate}
             exit={variants.exit}
